2d8b6fe7af96cb001d5b86c9905686b307c0107a
jimmay5469 <jimmay5469@gmail.com>
2019-01-14 21:53:15 -0500

    Show today/week/year with durations on athlete page

--- assets/js/components/Athlete.js
+++ assets/js/components/Athlete.js
@@ -1,7 +1,48 @@
 import React from 'react'
+import moment from 'moment'
+import _ from 'lodash'
 
-export default ({ athlete: { firstName, lastName } }) => (
-  <>
-    {firstName} {lastName}
-  </>
-)
+moment.relativeTimeThreshold('M', 12)
+moment.relativeTimeThreshold('d', 30)
+moment.relativeTimeThreshold('h', 24)
+moment.relativeTimeThreshold('m', 60)
+moment.relativeTimeThreshold('s', 60)
+moment.relativeTimeThreshold('ss', 1)
+
+const today = moment()
+const dayOfWeek = moment(today).isoWeekday()
+const dayOfYear = moment(today).dayOfYear()
+const startOfDay = moment(today).startOf('day')
+const startOfWeek = moment(today).startOf('isoWeek')
+const startOfYear = moment(today).startOf('year')
+
+const Duration = (seconds) => {
+  const minutes = Math.floor( seconds / 60 )
+  const hours = Math.floor( minutes / 60 )
+
+  return (
+    <>
+      {!!hours && `${hours}h `}{minutes % 60}m
+    </>
+  )
+}
+
+export default ({ athlete: { firstName, lastName, activities } }) => {
+  activities = _.map(activities, (activity) => ({ ...activity, day: moment(activity.startDateLocal.slice(0, 10)) }))
+  const todayActivities = _.filter(activities, ({ day }) => day.isBetween(startOfDay, today, 'day', '[]'))
+  const weekActivities = _.filter(activities, ({ day }) => day.isBetween(startOfWeek, today, 'day', '[]'))
+  const yearActivities = _.filter(activities, ({ day }) => day.isBetween(startOfYear, today, 'day', '[]'))
+
+  return (
+    <>
+      <h2>
+        {firstName} {lastName}
+      </h2>
+      <div>
+        Today: <input type='checkbox' disabled={true} checked={todayActivities.length > 0} /> ({Duration(_.sumBy(todayActivities, 'elapsedTime'))})&nbsp;
+        Week: {Object.keys(_.groupBy(weekActivities, 'day')).length}/{dayOfWeek} ({Duration(_.sumBy(weekActivities, 'elapsedTime'))})&nbsp;
+        Year: {Object.keys(_.groupBy(yearActivities, 'day')).length}/{dayOfYear} ({Duration(_.sumBy(yearActivities, 'elapsedTime'))})&nbsp;
+      </div>
+    </>
+  )
+}
