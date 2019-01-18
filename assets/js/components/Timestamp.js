905515cbc85195d775492bdf44cae1ac54331280
jimmay5469 <jimmay5469@gmail.com>
2019-01-18 07:53:34 -0500

    Refresh Timestamp fromNow every second

--- assets/js/components/Timestamp.js
+++ assets/js/components/Timestamp.js
@@ -1,4 +1,14 @@
 import React from 'react'
 import moment from 'moment'
 
-export default ({ value }) => moment(value).fromNow()
+export default class extends React.Component {
+  componentDidMount() {
+    this.interval = setInterval(() => this.forceUpdate(), 1000)
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+  render() {
+    return moment(this.props.value).fromNow()
+  }
+}
