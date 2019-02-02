579ec48039cf1505c506f99243165dd7b3ffd15b
jimmay5469 <jimmay5469@gmail.com>
2019-02-02 15:45:33 -0500

    Cleanup responsiveness

--- assets/js/components/Layout.js
+++ assets/js/components/Layout.js
@@ -18,38 +18,44 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 const Layout = ({ children, hamburgerMenuOpen, loginUrl, logoutUrl, stravaId, athletes, onHamburgerClick }) => (
-  <div>
+  <>
     <nav className='navbar has-shadow'>
-      <div className='navbar-brand'>
-        <div className='navbar-item'>
-          <h1 className='title has-text-weight-normal'>
-            <Link to='/'>The Daily Grind Club</Link>
-          </h1>
-        </div>
-        <a className={`navbar-burger ${hamburgerMenuOpen ? 'is-active' : ''}`} onClick={onHamburgerClick}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </a>
-      </div>
-      <div className={`navbar-menu ${hamburgerMenuOpen ? 'is-active' : ''}`}>
-        <div className='navbar-end'>
+      <div className='container'>
+        <div className='navbar-brand'>
           <div className='navbar-item'>
-            {stravaId && !!athletes.length && <Link to='/'>Home</Link>}
+            <h1 className='title has-text-weight-normal'>
+              <Link to='/'>The Daily Grind Club</Link>
+            </h1>
           </div>
-          <div className='navbar-item'>
-            {stravaId && !!athletes.length && <a href={logoutUrl}>Logout</a>}
+          <a className={`navbar-burger ${hamburgerMenuOpen ? 'is-active' : ''}`} onClick={onHamburgerClick}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+        </div>
+        <div className={`navbar-menu ${hamburgerMenuOpen ? 'is-active' : ''}`}>
+          <div className='navbar-end'>
+            <div className='navbar-item'>
+              {stravaId && !!athletes.length && <Link to='/'>Home</Link>}
+            </div>
+            <div className='navbar-item'>
+              {stravaId && !!athletes.length && <a href={logoutUrl}>Logout</a>}
+            </div>
           </div>
         </div>
       </div>
     </nav>
-    <main role='main' className='content'>
-      {!stravaId && <a href={loginUrl}>Login with Strava</a>}
-      {stravaId && !athletes.length && <div>You need to join <a href='https://www.strava.com/clubs/thedailygrindclub'>The Daily Grind Club</a> on Strava to use this site.</div>}
-      {stravaId && !athletes.length && <div>Already joined? <a href={loginUrl}>Try logging in with Strva again.</a></div>}
-      {children}
+    <main className='section'>
+      <div className='container'>
+        <div className='content'>
+          {!stravaId && <a href={loginUrl}>Login with Strava</a>}
+          {stravaId && !athletes.length && <div>You need to join <a href='https://www.strava.com/clubs/thedailygrindclub'>The Daily Grind Club</a> on Strava to use this site.</div>}
+          {stravaId && !athletes.length && <div>Already joined? <a href={loginUrl}>Try logging in with Strva again.</a></div>}
+          {children}
+        </div>
+      </div>
     </main>
-  </div>
+  </>
 )
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Layout))
