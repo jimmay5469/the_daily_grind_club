4a3576ce373c4e56e5d345825bb5828bd9b1504c
jimmay5469 <jimmay5469@gmail.com>
2019-02-06 22:36:26 -0500

    Make disconnected notification less alarming

--- assets/js/components/Layout.js
+++ assets/js/components/Layout.js
@@ -45,19 +45,12 @@ const Layout = ({ children, connected, hamburgerMenuOpen, loginUrl, logoutUrl, s
         </div>
       </div>
     </nav>
+    {!!athletes.length && !connected && <div className='notification is-radiusless is-marginless is-paddingless has-text-centered'>Refresh to get the latest activities.</div>}
     <main className='section'>
       <div className='container'>
         {!stravaId && <a href={loginUrl}>Login with Strava</a>}
         {stravaId && !athletes.length && <div>You need to join <a href='https://www.strava.com/clubs/thedailygrindclub'>The Daily Grind Club</a> on Strava to use this site.</div>}
         {stravaId && !athletes.length && <div>Already joined? <a href={loginUrl}>Try logging in with Strva again.</a></div>}
-        {!!athletes.length && !connected && <div className='columns is-centered is-mobile'>
-          <div className='column is-narrow'>
-            <div className='tags has-addons'>
-              <span className='tag is-danger'>Disconnected!</span>
-              <span className='tag'>refresh the page to reconnect</span>
-            </div>
-          </div>
-        </div>}
         {children}
       </div>
     </main>
