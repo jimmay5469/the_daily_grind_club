6fea399100e0339417d776f826c49bc490e37f6d
jimmay5469 <jimmay5469@gmail.com>
2019-02-09 15:31:18 -0500

    Run standard on build.js

--- assets/build.js
+++ assets/build.js
@@ -1,12 +1,12 @@
-const Bundler = require('parcel-bundler');
-const { execSync } = require('child_process');
-const fs = require('fs');
+const Bundler = require('parcel-bundler')
+const { execSync } = require('child_process')
+const fs = require('fs')
 
 const entryFiles = [
   './static/**',
   './js/app.js',
   './css/app.scss'
-];
+]
 
 const options = {
   outDir: '../priv/static/',
@@ -17,25 +17,25 @@ const options = {
 (async () => {
   cleanPrivStatic()
 
-  const bundler = new Bundler(entryFiles, options);
+  const bundler = new Bundler(entryFiles, options)
 
   bundler.on('buildEnd', () => {
     putStaticFilesInTheRightPlace()
     fixStaticFileReferences()
-  });
+  })
 
-  await bundler.bundle();
-})();
+  await bundler.bundle()
+})()
 
-function cleanPrivStatic() {
-  execSync('rm -rf ../priv/static/');
+function cleanPrivStatic () {
+  execSync('rm -rf ../priv/static/')
 }
-function fixStaticFileReferences() {
-  execSync(String.raw`find ../priv/static/css/ -maxdepth 1 -name *.css -exec sed -i '' -e 's/static\//..\//g' {} \;`);
+function fixStaticFileReferences () {
+  execSync(String.raw`find ../priv/static/css/ -maxdepth 1 -name *.css -exec sed -i '' -e 's/static\//..\//g' {} \;`)
 }
-function putStaticFilesInTheRightPlace() {
+function putStaticFilesInTheRightPlace () {
   if (fs.existsSync('../priv/static/static/')) {
-    execSync('mv ../priv/static/static/** ../priv/static/');
-    execSync('rm -rf ../priv/static/static/');
+    execSync('mv ../priv/static/static/** ../priv/static/')
+    execSync('rm -rf ../priv/static/static/')
   }
 }
