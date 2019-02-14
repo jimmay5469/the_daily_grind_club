281030a86da4535d49a2992372b76ea26067e47b
jimmay5469 <jimmay5469@gmail.com>
2019-02-14 11:12:43 -0500

    Namespace pages in sass

--- assets/js/components/AthleteListRoute.js
+++ assets/js/components/AthleteListRoute.js
@@ -54,61 +54,59 @@ const AthleteListRoute = ({
   athleteList,
   dayOfWeek
 }) => (
-  <div className='columns is-centered'>
+  <div className='athlete-list-route columns is-centered'>
     <div className='column is-three-quarters-widescreen'>
       {!!athleteList.length && <h3 className='title is-4'>Latest Activity</h3>}
       {athleteList.map(({ stravaId, firstName, lastName, latestActivity, todaySeconds, weekActiveDays, streak, yearSeconds, activityTypes }) => (
-        <div key={stravaId}>
+        <div key={stravaId} className='athlete-box box is-radiusless'>
           <div className='stripe'>
             {_.map(activityTypes, ({ type, seconds }) => (
               <div key={type} style={{ width: `${100 * seconds / yearSeconds}%`, backgroundColor: colorHash.hex(type) }} />
             ))}
           </div>
-          <div className='box is-radiusless'>
-            <div className='columns is-mobile'>
-              <div className='column'>
-                <strong><Link to={`/athletes/${stravaId}`} className='has-text-dark'>{firstName} {lastName}</Link></strong>
-              </div>
-              <div className='column is-narrow'>
-                {latestActivity && <Timestamp value={latestActivity.startDate} />}
+          <div className='columns is-mobile'>
+            <div className='column'>
+              <strong><Link to={`/athletes/${stravaId}`} className='has-text-dark'>{firstName} {lastName}</Link></strong>
+            </div>
+            <div className='column is-narrow'>
+              {latestActivity && <Timestamp value={latestActivity.startDate} />}
+            </div>
+          </div>
+          <div className='columns'>
+            <div className='column'>
+              <div className='field is-grouped is-grouped-multiline'>
+                <div className='control'>
+                  <div className='tags has-addons'>
+                    <span className='tag'>Today</span>
+                    {todaySeconds > 0
+                      ? <span className='tag is-success'><Duration seconds={todaySeconds} /></span>
+                      : <span className='tag is-dark'>No</span>
+                    }
+                  </div>
+                </div>
+                <div className='control'>
+                  <div className='tags has-addons'>
+                    <span className='tag'>Week</span>
+                    {weekActiveDays === dayOfWeek
+                      ? <span className='tag is-success'>{weekActiveDays}/{dayOfWeek}</span>
+                      : <span className='tag is-dark'>{weekActiveDays}/{dayOfWeek}</span>
+                    }
+                  </div>
+                </div>
+                {!!streak && <div className='control'>
+                  <div className='tags has-addons'>
+                    <span className='tag'>Streak</span>
+                    <span className='tag is-success'>{streak}</span>
+                  </div>
+                </div>}
               </div>
             </div>
-            <div className='columns'>
-              <div className='column'>
-                <div className='field is-grouped is-grouped-multiline'>
-                  <div className='control'>
-                    <div className='tags has-addons'>
-                      <span className='tag'>Today</span>
-                      {todaySeconds > 0
-                        ? <span className='tag is-success'><Duration seconds={todaySeconds} /></span>
-                        : <span className='tag is-dark'>No</span>
-                      }
-                    </div>
-                  </div>
-                  <div className='control'>
-                    <div className='tags has-addons'>
-                      <span className='tag'>Week</span>
-                      {weekActiveDays === dayOfWeek
-                        ? <span className='tag is-success'>{weekActiveDays}/{dayOfWeek}</span>
-                        : <span className='tag is-dark'>{weekActiveDays}/{dayOfWeek}</span>
-                      }
-                    </div>
-                  </div>
-                  {!!streak && <div className='control'>
-                    <div className='tags has-addons'>
-                      <span className='tag'>Streak</span>
-                      <span className='tag is-success'>{streak}</span>
-                    </div>
-                  </div>}
+            <div className='column is-narrow'>
+              {_.chain(activityTypes).take(3).map(({ type, seconds }) => (
+                <div key={type} className='athlete-type-list'>
+                  <div className='type-swatch' style={{ backgroundColor: colorHash.hex(type) }} /> {_.words(type).join(' ')}
                 </div>
-              </div>
-              <div className='column is-narrow'>
-                {_.chain(activityTypes).take(3).map(({ type, seconds }) => (
-                  <div key={type} className='horizontal-type-list-item'>
-                    <div className='type-swatch' style={{ backgroundColor: colorHash.hex(type) }} /> {_.words(type).join(' ')}
-                  </div>
-                )).value()}
-              </div>
+              )).value()}
             </div>
           </div>
         </div>
